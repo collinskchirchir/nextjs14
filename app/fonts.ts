2cb80283ddadbe985872c58837349b53fe7d381c
Collins Chirchir <collinskchirchir@gmail.com>
2024-01-05 22:36:29 +0300

    customized layout

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,16 +1,21 @@
 // import { Inter, Lexend_Deca } from 'next/font/google';
-import localFonts from "next/font/local";
+import localFonts from 'next/font/local';
 
 export const inter = localFonts({
-  src: "./fonts/StudioFeixenSansVF.ttf",
-  variable: "--font-inter",
+  src: './fonts/StudioFeixenSansVF.ttf',
+  variable: '--font-inter',
+});
+
+export const spaceGrotesk = localFonts({
+  src: './fonts/SpaceGrotesk-VariableFont_wght.ttf',
+  variable: '--font-spaceGrotesk',
 });
 
 export const jetBrains = localFonts({
-  src: "./fonts/JetBrainsMono-VariableFont_wght.ttf",
-  variable: "--font-jetbrains",
+  src: './fonts/JetBrainsMono-VariableFont_wght.ttf',
+  variable: '--font-jetbrains',
 });
 export const feixenMono = localFonts({
-  src: "./fonts/StudioFeixenMono.ttf",
-  variable: "--font-feixen-mono",
+  src: './fonts/StudioFeixenMono.ttf',
+  variable: '--font-feixen-mono',
 });
