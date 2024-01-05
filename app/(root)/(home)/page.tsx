33f99067b19b8eb1e002c00cdd961fa5fb84ee4a
Collins Chirchir <collinskchirchir@gmail.com>
2024-01-05 14:41:23 +0300

    clerk auth config

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='flex p-1'>
+      <UserButton afterSignOutUrl='/' />
+      <h2>Home</h2>
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
 import { feixenMono, inter, jetBrains } from "@/app/fonts";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,12 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.variable} ${jetBrains.variable} ${feixenMono.variable}`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${inter.variable} ${jetBrains.variable} ${feixenMono.variable}`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
