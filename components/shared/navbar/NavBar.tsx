cca7d59179ad32005371ba53e97317d0a042e382
Collins Chirchir <collinskchirchir@gmail.com>
2024-01-06 13:34:59 +0300

    container and user account

---
+++ components/shared/navbar/NavBar.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+export default function NavBar() {
+  return (
+    <nav className='flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 sm:px-12 dark:shadow-none'>
+      <Link href='/' className='flex items-center gap-1'>
+        <Image
+          src='/assets/images/site-logo.svg'
+          alt='DevFlow'
+          width={23}
+          height={23}
+        />
+        <p className='h2-bold font-spaceGrotesk text-dark-100 max-sm:hidden dark:text-light-900'>
+          Dev <span className='text-primary-500'>OverFlow</span>
+        </p>
+      </Link>
+      Global Search
+      <div className='flex-between gap-5'>
+        Theme
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl='/'
+            appearance={{
+              elements: {
+                avatarBox: 'h-10 w-10',
+              },
+              variables: {
+                colorPrimary: '#ff7000',
+              },
+            }}
+          />
+        </SignedIn>
+        MobileNav
+      </div>
+    </nav>
+  );
+}
