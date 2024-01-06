11c7216573c4b376c669c9ebc0b0faa02c6ec933
Collins Chirchir <collinskchirchir@gmail.com>
2024-01-06 11:58:08 +0300

    created global theme context

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@ import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <div className='flex p-1'>
+    <div>
       <UserButton afterSignOutUrl='/' />
       <h2>Home</h2>
     </div>
