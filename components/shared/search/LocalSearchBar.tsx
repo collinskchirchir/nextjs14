345900ccd8c4bd56e52fe9ece0b70229cb7b9c8c
Collins Chirchir <collinskchirchir@gmail.com>
2024-01-16 15:14:48 +0300

    LocalSearchbar done

---
+++ components/shared/search/LocalSearchBar.tsx
@@ -0,0 +1,53 @@
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import { Input } from '@/components/ui/input';
+
+interface Props {
+  route: string;
+  iconPosition: string;
+  imgSrc: string;
+  placeholder?: string;
+  otherClasses?: string;
+}
+
+export default function LocalSearchbar({
+  route,
+  iconPosition,
+  imgSrc,
+  placeholder,
+  otherClasses,
+}: Props) {
+  return (
+    <div
+      className={cn(
+        'background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4',
+        otherClasses
+      )}
+    >
+      {iconPosition === 'left' && (
+        <Image
+          src={imgSrc}
+          alt='search icon'
+          width={24}
+          height={24}
+          className='cursor-pointer'
+        />
+      )}
+      <Input
+        type='text'
+        placeholder={placeholder}
+        value=''
+        className='paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none'
+      />
+      {iconPosition === 'right' && (
+        <Image
+          src={imgSrc}
+          alt='search icon'
+          width={24}
+          height={24}
+          className='cursor-pointer'
+        />
+      )}
+    </div>
+  );
+}
