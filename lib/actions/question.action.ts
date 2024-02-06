5ddcd6c509e4097775ac52b3813a45e2723a823e
Collins Chirchir <collinskchirchir@gmail.com>
2024-02-06 21:20:54 +0300

    halfway creating Question server action

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,10 +1,36 @@
 'use server';
 
 import { connectToDatabase } from '@/lib/mongoose';
+import Question from '@/database/question.model';
+import Tag from '@/database/tag.model';
 
 export async function createQuestion(params: any) {
   try {
     // connect to DB
     connectToDatabase();
+    const { title, content, tags, author, path } = params;
+    // create the question
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+    const tagDocuments = [];
+    // Create the tags or get them if they already exist
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, 'i') } },
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { upsert: true, new: true }
+      );
+      tagDocuments.push(existingTag._id);
+    }
+
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
+    // TODO: Create an interaction record for the user's ask_question action
+
+    // TODO: Increment author's reputation by +5 for creating a question
   } catch (error) {}
 }
