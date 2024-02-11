5c71d9e1f8fdde2b697c44060e59d69a752c2338
Collins Chirchir <collinskchirchir@gmail.com>
2024-02-11 18:48:58 +0300

    added getQuestions server action

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -3,8 +3,29 @@
 import { connectToDatabase } from '@/lib/mongoose';
 import Question from '@/database/question.model';
 import Tag from '@/database/tag.model';
+import {
+  CreateQuestionParams,
+  GetQuestionsParams,
+} from '@/lib/actions/shared.types';
+import User from '@/database/user. model';
+import { revalidatePath } from 'next/cache';
+import { sort } from 'next/dist/build/webpack/loaders/css-loader/src/utils';
 
-export async function createQuestion(params: any) {
+export async function getQuestions(params: GetQuestionsParams) {
+  try {
+    connectToDatabase();
+    const questions = await Question.find({})
+      .populate({ path: 'tags', model: Tag })
+      .populate({ path: 'author', model: User })
+      .sort({ createdAt: -1 });
+    return { questions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createQuestion(params: CreateQuestionParams) {
   try {
     // connect to DB
     connectToDatabase();
@@ -33,5 +54,7 @@ export async function createQuestion(params: any) {
     // TODO: Create an interaction record for the user's ask_question action
 
     // TODO: Increment author's reputation by +5 for creating a question
+
+    revalidatePath(path);
   } catch (error) {}
 }
