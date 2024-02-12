20090b53499104147825f049a3c0fa304fbc0976
Collins Chirchir <collinskchirchir@gmail.com>
2024-02-12 19:23:47 +0300

    replaced dummy clerkID with real one

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,10 +1,11 @@
 import Question from '@/components/forms/Question';
 import { redirect } from 'next/navigation';
 import { getUserById } from '@/lib/actions/user.action';
+import { auth } from '@clerk/nextjs';
 
 const AskQuestion = async () => {
-  // const { userId } = auth();
-  const userId = 'clerk123';
+  const { userId } = auth();
+
   if (!userId) redirect('/sign-in');
 
   const mongoUser = await getUserById({ userId });
