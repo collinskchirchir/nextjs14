fd8153450787bfe8bdb3640f119fb1d5be40e8fb
Collins Chirchir <collinskchirchir@gmail.com>
2024-02-12 12:20:39 +0300

    minor type fix

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function Page() {
   return (
     <div className='flex justify-center p-24'>
-      <SignUp />;{' '}
+      <SignUp />{' '}
     </div>
   );
 }
