c59891963ab82cc00458a82449859e0e1f74a81a
Collins Chirchir <collinskchirchir@gmail.com>
2024-02-28 23:01:06 +0300

    Answer form component partial

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -6,6 +6,7 @@ import Metric from '@/components/shared/Metric';
 import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
 import ParseHTML from '@/components/shared/ParseHTML';
 import RenderTag from '@/components/shared/RenderTag';
+import Answer from '@/components/forms/Answer';
 
 interface QuestionPageParams {
   params: { id: string };
@@ -79,6 +80,9 @@ const QuestionPage = async ({ params }: QuestionPageParams) => {
           />
         ))}
       </div>
+
+      {/* Answer Form */}
+      <Answer />
     </>
   );
 };
