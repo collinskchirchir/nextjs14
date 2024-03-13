b0473feeda26541e79d0bea8b8bc8d1f77190333
Collins Chirchir <collinskchirchir@gmail.com>
2024-03-13 15:28:57 +0300

    feature - display saved questions in Collection

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
+import Filter from '@/components/shared/Filter';
+import { QuestionFilters } from '@/constants/filters';
+import QuestionCard from '@/components/cards/QuestionCard';
+import NoResult from '@/components/shared/NoResult';
+import { getSavedQuestions } from '@/lib/actions/user.action';
+import { auth } from '@clerk/nextjs';
+
+const Collection = async () => {
+  const { userId } = auth();
+  if (!userId) return null;
+  const result = await getSavedQuestions({
+    clerkId: userId,
+  });
+  console.log('Collection Results:', JSON.stringify(result));
+  return (
+    <>
+      <h1 className='h1-bold text-dark100_light900'>Saved Questions</h1>
+
+      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
+        <LocalSearchbar
+          route='/'
+          iconPosition='left'
+          imgSrc='/assets/icons/search.svg'
+          placeholder='Search for questions'
+          otherClasses='flex-1'
+        />
+        <Filter
+          filters={QuestionFilters}
+          otherClasses='min-h-[56px] sm:min-w-[170px]'
+        />
+      </div>
+
+      {/*  QUESTION CARD section */}
+      <div className='mt-10 flex w-full flex-col gap-6'>
+        {/*  looping through questions */}
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no saved question to show"
+            description='Be the first to break the silence! Ask a Question and kickstart the discussion. Our query could be the next big thing others learn from. Get involved!'
+            link='/'
+            linkTitle='Save a Question'
+          />
+        )}
+      </div>
+      {/*  END OF QUESTION CARD section */}
+    </>
+  );
+};
+
+export default Collection;
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,11 +5,14 @@ import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
+  GetSavedQuestionsParams,
   ToggleSaveQuestionParams,
   UpdateUserParams,
 } from '@/lib/actions/shared.types';
 import { revalidatePath } from 'next/cache';
 import Question from '@/database/question.model';
+import Tag from '@/database/tag.model';
+import { FilterQuery } from 'mongoose';
 
 export async function getUserById(params: any) {
   try {
@@ -116,6 +119,37 @@ export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
       );
     }
     revalidatePath(path);
+  } catch (error) {
+    console.error(`❌ ${error} ❌`);
+    throw error;
+  }
+}
+
+export async function getSavedQuestions(params: GetSavedQuestionsParams) {
+  try {
+    await connectToDatabase();
+
+    // eslint-disable-next-line no-unused-vars
+    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, 'i') } }
+      : {};
+    const user = await User.findOne({ clerkId }).populate({
+      path: 'saved',
+      match: query,
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: 'tags', model: Tag, select: '_id name' },
+        { path: 'author', model: User, select: '_id clerkId name picture' },
+      ],
+    });
+    if (!user) {
+      throw new Error('❌ User not found ❌');
+    }
+    const savedQuestions = user.saved;
+    return { questions: savedQuestions };
   } catch (error) {
     console.error(`❌ ${error} ❌`);
     throw error;
