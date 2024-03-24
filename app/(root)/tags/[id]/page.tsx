d4345331dea7f30eb3ad92cae8380ce3aedc8050
Collins Chirchir <collinskchirchir@gmail.com>
2024-03-24 20:58:39 +0300

    create a tag details page

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { getQuestionsByTagId } from '@/lib/actions/tag.actions';
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
+import QuestionCard from '@/components/cards/QuestionCard';
+import NoResult from '@/components/shared/NoResult';
+import { URLProps } from '@/types';
+
+const TagDetailsPage = async ({ params, searchParams }: URLProps) => {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    searchQuery: searchParams.q,
+  });
+  console.log(result);
+  return (
+    <>
+      <h1 className='h1-bold text-dark100_light900'>
+        {result && result.tagTitle}
+      </h1>
+
+      <div className='mt-11 w-full'>
+        <LocalSearchbar
+          route='/'
+          iconPosition='left'
+          imgSrc='/assets/icons/search.svg'
+          placeholder='Search for tag questions'
+          otherClasses='flex-1'
+        />
+      </div>
+
+      {/*  QUESTION CARD section */}
+      <div className='mt-10 flex w-full flex-col gap-6'>
+        {/*  looping through questions */}
+        {result && result.questions && result.questions.length > 0 ? (
+          result.questions.map((question: any) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no tag question to show"
+            description='Be the first to break the silence! Ask a Question and kickstart the discussion. Our query could be the next big thing others learn from. Get involved!'
+            link='/'
+            linkTitle='Save a Question'
+          />
+        )}
+      </div>
+      {/*  END OF QUESTION CARD section */}
+    </>
+  );
+};
+
+export default TagDetailsPage;
