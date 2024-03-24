846146e413a3a92e1fe9a3009ca4481ac4bac995
Collins Chirchir <collinskchirchir@gmail.com>
2024-03-24 20:53:30 +0300

    fix - question object not being pushed on tags collection

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,11 +1,14 @@
 'use server';
 import {
   GetAllTagsParams,
+  GetQuestionsByTagIdParams,
   GetTopInteractedTagsParams,
 } from '@/lib/actions/shared.types';
 import { connectToDatabase } from '@/lib/mongoose';
 import User from '@/database/user. model';
-import Tag from '@/database/tag.model';
+import Tag, { ITag } from '@/database/tag.model';
+import { FilterQuery } from 'mongoose';
+import Question from '@/database/question.model';
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -34,5 +37,35 @@ export async function getAllTags(params: GetAllTagsParams) {
   } catch (error) {
     console.error(`❌ ${error} ❌`);
     throw error;
+  }
+}
+
+export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
+  try {
+    await connectToDatabase();
+    // eslint-disable-next-line no-unused-vars
+    const { tagId, searchQuery, page = 1, pageSize = 10 } = params;
+    const tagFilter: FilterQuery<ITag> = { _id: tagId };
+    const tag = await Tag.findOne(tagFilter).populate({
+      path: 'questions',
+      model: Question,
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: 'i' } }
+        : {},
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: 'tags', model: Tag, select: '_id name' },
+        { path: 'author', model: User, select: '_id clerkId name picture' },
+      ],
+    });
+    if (!tag) {
+      throw new Error('❌ Tag not found ❌');
+    }
+    const questions = tag.questions;
+    return { tagTitle: tag.name, questions };
+  } catch (error) {
+    console.error(`❌ ${error} ❌`);
   }
 }
