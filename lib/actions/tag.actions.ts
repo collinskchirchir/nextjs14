29084922b226766eecf33cdc7b88a214efd4a0be
Collins Chirchir <collinskchirchir@gmail.com>
2024-04-29 16:58:11 +0300

    implement popular tags

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -69,3 +69,17 @@ export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
     console.error(`❌ ${error} ❌`);
   }
 }
+
+export async function getTopPopularTags() {
+  try {
+    await connectToDatabase();
+    const popularTags = await Tag.aggregate([
+      { $project: { name: 1, numberOfQuestions: { $size: '$questions' } } },
+      { $sort: { numberOfQuestions: -1 } }, // descending order
+      { $limit: 5 },
+    ]);
+    return popularTags;
+  } catch (error) {
+    console.error(`❌ ${error} ❌`);
+  }
+}
