60b3bb5c1f43ae685e3cb7463536fa1739a589a5
Collins Chirchir <collinskchirchir@gmail.com>
2024-05-07 21:49:37 +0300

    created global search result component

---
+++ components/shared/search/GlobalFilters.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GlobalFilters = () => {
+  return (
+    <div>
+      <p className='text-dark400_light900 paragraph-semibold px-5'>
+        Global Filters
+      </p>
+    </div>
+  );
+};
+
+export default GlobalFilters;
